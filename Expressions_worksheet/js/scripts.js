77796cc4814aa2728afe3559bc313d64270188ab
Gregory Mitchell <gpmitchell27@fullsail.edu>
2014-02-13 16:38:45 -0500

    Expressions worksheet
    First problem solved

---
+++ Expressions_worksheet/js/scripts.js
@@ -0,0 +1,6 @@
+// Gregory Mitchell Feb 13, 2014 Activity: Expression Worksheet
+
+var age = 3;
+var result= age * 7;
+
+console.log("Sparky is " + age + " human years old which is " + result + " in dog years" );
