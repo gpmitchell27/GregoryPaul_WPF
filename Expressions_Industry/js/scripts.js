757c14a808d34156ea44d420c6973a09d59b5a0d
Gregory Mitchell <gpmitchell27@fullsail.edu>
2014-02-13 22:12:08 -0500

    array defined
    parseInt added to prompt for string conversion

--- Expressions_Industry/js/scripts.js
+++ Expressions_Industry/js/scripts.js
@@ -1,14 +1,20 @@
 //Gregory Mitchell Feb 13, 2014 Activity: Expression Personal
 //Collect user input to calculate the width of any element in HTML
+var contentWidthInput = parseInt(prompt("Have you ever wondered how to properly calculate the total width of an element in HTML?\nEnter in some quick data and I will calculate it for you! \n\nWhat is the width of your content?"));
+var contentLeftPaddingInput = parseInt(prompt("Input Left Padding?"));
+var contentRightPaddingInput = parseInt(prompt("Input Right Padding?"));
+var contentBorderLeftInput = parseInt(prompt("Input Left Border"));
+var contentBorderRightInput = parseInt(prompt("Input Right Border"));
+var contentMarginLeft = parseInt(prompt("Input Left Margin"));
+var contentMarginRight = parseInt(prompt("Input Right Margin"));
 var calculations = new Array();
-calculations.push(totalWidth);
-var contentWidthInput = prompt("Have you ever wondered how to properly calculate the total width of an element in HTML?\nEnter in some quick data and I will calculate it for you! \n\nWhat is the width of your content?");
-var contentLeftPaddingInput = prompt("Input Left Padding?");
-var contentRightPaddingInput = prompt("Input Right Padding?");
-var contentBorderLeftInput = prompt("Input Left Border");
-var contentBorderRightInput = prompt("Input Right Border");
-var contentMarginLeft = prompt("Input Left Margin");
-var contentMarginRight = prompt("Input Right Margin");
-var totalWidth = contentWidthInput + contentLeftPaddingInput + contentRightPaddingInput + contentBorderLeftInput + contentBorderRightInput + contentMarginLeft + contentMarginRight;
-
+calculations.push(contentWidthInput);
+calculations.push(contentLeftPaddingInput);
+calculations.push(contentRightPaddingInput);
+calculations.push(contentBorderLeftInput);
+calculations.push(contentBorderRightInput);
+calculations.push(contentMarginLeft);
+calculations.push(contentMarginRight);
 console.log(calculations);
+var totalWidth = calculations[0] + calculations[1] + calculations[2] + calculations[3] + calculations[4] + calculations[5] + calculations[6];
+console.log(totalWidth);
