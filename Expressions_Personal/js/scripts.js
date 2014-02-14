944a929098cc76eb4af42db03473f150f5b39a1c
Gregory Mitchell <gpmitchell27@fullsail.edu>
2014-02-13 19:25:26 -0500

    First prompt
    First prompt for user input

--- Expressions_Personal/js/scripts.js
+++ Expressions_Personal/js/scripts.js
@@ -1,2 +1,3 @@
 //Gregory Mitchell Feb 13, 2014 Activity: Expression Personal
+var daysInput = prompt("I am collecting information to calculate how many repititions people do per week at the gym.\n\nHow many times per week do you workout?");
 
