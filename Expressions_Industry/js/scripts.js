09653ce9861e2aae89e262531ba98ca66773f4ae
Gregory Mitchell <gpmitchell27@fullsail.edu>
2014-02-13 21:11:46 -0500

    adjusted variables
    instead of collecting data in an array - variables for each user input

--- Expressions_Industry/js/scripts.js
+++ Expressions_Industry/js/scripts.js
@@ -1,6 +1,10 @@
 //Gregory Mitchell Feb 13, 2014 Activity: Expression Personal
 //Collect user input to calculate the width of any element in HTML
 var contentWidthInput = prompt("Have you ever wondered how to properly calculate the total width of an element in HTML?\nEnter in some quick data and I will calculate it for you! \n\nWhat is the width of your content?");
-var contentPaddingInput = prompt("What is the padding on the element if any at all?");
-var input[] = prompt("Does the element have a border on the left or right?");
-var co
+var contentLeftPaddingInput = prompt("Input Left Padding?");
+var contentRightPaddingInput = prompt("Input Right Padding?");
+var contentBorderInput = prompt("Input Left Border");
+var contentBorderInput = prompt("Input Right Border");
+console.log(contentWidthInput);
+console.log(contentPaddingInput);
+console.log(contentBorderInput);
