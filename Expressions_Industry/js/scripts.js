ca5ea1fba69b5de577bff8fd697006cf1531088b
Gregory Mitchell <gpmitchell27@fullsail.edu>
2014-02-13 20:09:23 -0500

    Third prompt
    user prompt asking for border of element

--- Expressions_Industry/js/scripts.js
+++ Expressions_Industry/js/scripts.js
@@ -2,3 +2,5 @@
 //Collect user input to calculate the width of any element in HTML
 var contentWidthInput = prompt("Have you ever wondered how to properly calculate the total width of an element in HTML?\nEnter in some quick data and I will calculate it for you! \n\nWhat is the width of your content?");
 var contentPaddingInput = prompt("What is the padding on the element if any at all?");
+var contentBorderInput = prompt("Does the element have a border on the left or right?");
+var co
