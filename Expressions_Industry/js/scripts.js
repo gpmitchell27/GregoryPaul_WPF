dd7bbc6869562e7d68d1ed30b3bddfe49ae0cd93
Gregory Mitchell <gpmitchell27@fullsail.edu>
2014-02-13 22:39:44 -0500

    total width calculated
    total width calculated

--- Expressions_Industry/js/scripts.js
+++ Expressions_Industry/js/scripts.js
@@ -17,4 +17,3 @@ calculations.push(contentMarginLeft);
 calculations.push(contentMarginRight);
 console.log(calculations);
 var totalWidth = calculations[0] + calculations[1] + calculations[2] + calculations[3] + calculations[4] + calculations[5] + calculations[6];
-console.log(totalWidth);
