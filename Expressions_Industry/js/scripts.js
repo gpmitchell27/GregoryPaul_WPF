310278d016ae4440a63e4a7552115df38d581434
Gregory Mitchell <gpmitchell27@fullsail.edu>
2014-02-13 23:40:16 -0500

    Expression Wacky
    Wacky expression using switch statements to create a full sentence with
    different outcomes depending on which is chosen by the user

--- Expressions_Industry/js/scripts.js
+++ Expressions_Industry/js/scripts.js
@@ -1,6 +1,6 @@
 //Gregory Mitchell Feb 13, 2014 Activity: Expression Industry
 //Collect user input to calculate the width of any element in HTML
-var contentWidthInput = parseInt(prompt("Have you ever wondered how to properly calculate the total width of an element in HTML?\nEnter in some quick data and I will calculate it for you! \n\nWhat is the width of your content?")); // ask user for width of content and opening message -- all inputs have parseInt to convert user strings to int
+var contentWidthInput = prompt("Have you ever wondered how to properly calculate the total width of an element in HTML?\nEnter in some quick data and I will calculate it for you! \n\nWhat is the width of your content?"); // ask user for width of content and opening message -- all inputs have parseInt to convert user strings to int
 var contentLeftPaddingInput = parseInt(prompt("Input Left Padding?")); // prompt for left padding
 var contentRightPaddingInput = parseInt(prompt("Input Right Padding?")); // prompt for right padding
 var contentBorderLeftInput = parseInt(prompt("Input Left Border")); // prompt for left border
--- Expressions_Wacky/js/scripts.js
+++ Expressions_Wacky/js/scripts.js
@@ -1,8 +1,36 @@
 //Gregory Mitchell Feb 13, 2014 Activity: Expression Wacky
-
-var yearBorn = 1955;
-
-/*console.log("Hello World");
-console.log(yearBorn);*/
-
-alert(yearBorn);
+// 
+var game = prompt("What's your favorite game series, halo , call of duty, or battlefild?", "halo");
+switch (game)
+{
+  case "halo" :
+     alert(game + " costs $42.50");
+     break;
+  case "call of duty" :
+     alert(game + " costs $43.00");
+     break;
+  case "battleField" :
+     alert(game + " costs $49.50");
+     break;
+  default :
+     alert("I don't recognize that game refresh page");
+     break;
+}
+var time = prompt("How many hours a day do you play?", "1 hours");
+switch (time)
+{
+  case "1 hours" :
+     alert(time + " Your brain is productive still");
+     break;
+  case "2 hours" :
+     alert(time + " A little more time playing and you will be a zombie");
+     break;
+  case "3 hours" :
+     alert(time + " Your wasting your life away");
+     break;
+  default :
+     alert("That's not a time");
+     break;
+}
+var name = prompt("What's your name?");
+console.log("Hey " + name + " you spend " + time + " a day playing " + game + ". Mom's say that if you play more then 1 hour a day you are wasting your life away, JUST SAYING");
