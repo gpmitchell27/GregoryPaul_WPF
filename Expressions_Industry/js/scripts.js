b98da58066b369d005b96a74829e4212c5f4321a
Gregory Mitchell <gpmitchell27@fullsail.edu>
2014-02-13 20:06:46 -0500

    Rework code
    Code lines were shifted to get rid of error "unterminated string
    litteral

--- Expressions_Industry/js/scripts.js
+++ Expressions_Industry/js/scripts.js
@@ -1,5 +1,3 @@
 //Gregory Mitchell Feb 13, 2014 Activity: Expression Personal
 //Collect user input to calculate the width of any element in HTML
-var contentWidthInput = prompt("Have you ever wondered how to properly calculate the total width of an element in HTML? \n 
-	Enter in some quick data and I'll calculate it for you! \n\n
-	What is the width of your content?");
+var contentWidthInput = prompt("Have you ever wondered how to properly calculate the total width of an element in HTML?\nEnter in some quick data and I will calculate it for you! \n\nWhat is the width of your content?");
