4c988507745eef16b55a68dad1345d2d26aa4ebc
Gregory Mitchell <gpmitchell27@fullsail.edu>
2014-02-13 20:07:54 -0500

    Second user prompt
    Second user prompt requiring padding data

--- Expressions_Industry/js/scripts.js
+++ Expressions_Industry/js/scripts.js
@@ -1,3 +1,4 @@
 //Gregory Mitchell Feb 13, 2014 Activity: Expression Personal
 //Collect user input to calculate the width of any element in HTML
 var contentWidthInput = prompt("Have you ever wondered how to properly calculate the total width of an element in HTML?\nEnter in some quick data and I will calculate it for you! \n\nWhat is the width of your content?");
+var contentPaddingInput = prompt("What is the padding on the element if any at all?");
