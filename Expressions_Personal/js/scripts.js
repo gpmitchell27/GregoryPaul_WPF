8cc6b89a1efa0b95dc89ee60aeaf03705300c58d
Gregory Mitchell <gpmitchell27@fullsail.edu>
2014-02-13 19:29:55 -0500

    new variable totalsets
    Total set variable calculated

--- Expressions_Personal/js/scripts.js
+++ Expressions_Personal/js/scripts.js
@@ -2,4 +2,4 @@
 var daysInput = prompt("I am collecting information to calculate how many repititions people do per week at the gym.\n\nHow many times per week do you workout?");
 var numberofExerciseInput = prompt("How many exercises do you do while at the gym?");
 var numberofSetsInput = prompt("How many sets total for all exercises?");
-var 
+var totalRepsPerDay = numberofExerciseInput * numberofSetsInput;
