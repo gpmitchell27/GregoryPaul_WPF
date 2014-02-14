f547a83f2d8390fb6d3f9018b7bbd24c35f70c68
Gregory Mitchell <gpmitchell27@fullsail.edu>
2014-02-13 22:48:08 -0500

    first comment
    first comment needs information about parseInt and what it does in the
    code

--- Expressions_Industry/js/scripts.js
+++ Expressions_Industry/js/scripts.js
@@ -1,6 +1,6 @@
 //Gregory Mitchell Feb 13, 2014 Activity: Expression Personal
 //Collect user input to calculate the width of any element in HTML
-var contentWidthInput = parseInt(prompt("Have you ever wondered how to properly calculate the total width of an element in HTML?\nEnter in some quick data and I will calculate it for you! \n\nWhat is the width of your content?")); // ask user for width of content and opening message
+var contentWidthInput = parseInt(prompt("Have you ever wondered how to properly calculate the total width of an element in HTML?\nEnter in some quick data and I will calculate it for you! \n\nWhat is the width of your content?")); // ask user for width of content and opening message -- all inputs have parseInt to convert user strings to int
 var contentLeftPaddingInput = parseInt(prompt("Input Left Padding?")); // prompt for left padding
 var contentRightPaddingInput = parseInt(prompt("Input Right Padding?")); // prompt for right padding
 var contentBorderLeftInput = parseInt(prompt("Input Left Border")); // prompt for left border
@@ -8,7 +8,7 @@ var contentBorderRightInput = parseInt(prompt("Input Right Border")); // prompt 
 var contentMarginLeft = parseInt(prompt("Input Left Margin")); // prompt for left margin
 var contentMarginRight = parseInt(prompt("Input Right Margin")); // prompt for right margin
 var calculations = new Array(); // populate array with inputted data
-calculations.push(contentWidthInput);
+calculations.push(contentWidthInput); // push all values to array
 calculations.push(contentLeftPaddingInput);
 calculations.push(contentRightPaddingInput);
 calculations.push(contentBorderLeftInput);
