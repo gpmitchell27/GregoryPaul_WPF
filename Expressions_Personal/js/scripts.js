e63d4ca924eda1fe4588eccf24d61ef7e85ad16e
Gregory Mitchell <gpmitchell27@fullsail.edu>
2014-02-13 19:41:32 -0500

    comments
    code commented for each line

--- Expressions_Personal/js/scripts.js
+++ Expressions_Personal/js/scripts.js
@@ -3,7 +3,7 @@
 var daysInput = prompt("I am collecting information to calculate how many repititions people do per week at the gym.\n\nHow many times per week do you workout?");
 var numberofExerciseInput = prompt("How many exercises do you do while at the gym?");
 var numberofSetsInput = prompt("How many sets total for all exercises?");
-var totalRepsPerDay = numberofExerciseInput * numberofSetsInput;
-console.log(totalRepsPerDay);
-var totalRepsPerWeek = totalRepsPerDay * daysInput;
-console.log("Based on your answers you do " + totalRepsPerWeek +  " total reps per week at the gym! WOW")
+var totalRepsPerDay = numberofExerciseInput * numberofSetsInput; // multiply number of exercises by number of sets
+//console.log(totalRepsPerDay) test for reps per day; 
+var totalRepsPerWeek = totalRepsPerDay * daysInput; // total reps * daysInput for result
+console.log("Based on your answers you do " + totalRepsPerWeek +  " total reps per week at the gym! WOW"); // Message to user after calculation is complete
