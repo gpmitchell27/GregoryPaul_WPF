4ea3960c34733b02a47d40f99a488d21a29e01ae
Gregory Mitchell <gpmitchell27@fullsail.edu>
2014-02-13 19:33:48 -0500

    new comment
    New comment to describe scope of project

--- Expressions_Personal/js/scripts.js
+++ Expressions_Personal/js/scripts.js
@@ -1,4 +1,5 @@
 //Gregory Mitchell Feb 13, 2014 Activity: Expression Personal
+//Collect user input for calculating reps per week
 var daysInput = prompt("I am collecting information to calculate how many repititions people do per week at the gym.\n\nHow many times per week do you workout?");
 var numberofExerciseInput = prompt("How many exercises do you do while at the gym?");
 var numberofSetsInput = prompt("How many sets total for all exercises?");
