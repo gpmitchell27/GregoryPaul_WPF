f7cd0c8823abe18140a689f40627de28fef2840a
Gregory Mitchell <gpmitchell27@fullsail.edu>
2014-02-20 22:21:31 -0500

    first variable
    setup first variable asking for name

--- Conditionals_Industry/js/scripts.js
+++ Conditionals_Industry/js/scripts.js
@@ -1,2 +1,8 @@
 //Gregory Mitchell Feb 20, 2014 Activity: Conditionals Industry
-//Collect user info 
+//Collect user info and change the color of the screen
+var who = prompt("What is your name");
+if (who == "") {
+	alert("You must enter a name");
+}
+
+
