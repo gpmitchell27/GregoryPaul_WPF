7e8755755499568c600c17b9fce19d95283a04dc
Gregory Mitchell <gpmitchell27@fullsail.edu>
2014-02-21 00:09:01 -0500

    changed to if
    needed if conditionals instead of switch

--- Conditionals_Wacky/js/scripts.js
+++ Conditionals_Wacky/js/scripts.js
@@ -2,34 +2,28 @@
 // wacky conditionals body fat index 
 var name = prompt("Please enter your name");
 var time = parseInt(prompt("On the average, how long does it take you to get ready in the morning?\nTen Minutes, Twenty Minutes, Thrity Minutes, Foutry Minutes, Fifty Minutes, Sixty Plus\nEnter the number in the box"));
-if(time <= 1 || >= 100) {
-  Math.round(time, 10);
-}
-switch (time) {
-  case 10:
+if(time < 1 || time < 9) {
   console.log(time + " minutes is very quick " + name + " there is no way you are female");
-  break;
-
-  case 20:
-  console.log(time + " minutes is still very quick " + name + " you are most likely a male");
-  break;
-
-  case 30:
-  console.log(time + " minutes is average " + name + " you could be a male or female");
-  break;
-
-  case 40:
-  console.log(time + " minutes is teetering on rediculous " + name + " you're most likely a female");
-  break;
-
-  case 50:
-  console.log(time + " is way too long to get ready " + name + " you scream female");
-  break;
-
-  case 60:
-  console.log(time + " really???? " + name + " do you get lost getting ready?");
-  break; 
 }
+if(time < 10 || time < 19) {
+  console.log(time + " minutes is still very quick " + name + " you are most likely a male");
+}
+if(time < 20 || time < 29) {
+  console.log(time + " minutes is still very quick " + name + " you are most likely a male");
+}
+if(time < 30 || time < 39) {
+  console.log(time + " minutes is average " + name + " you could be a male or female");
+}
+if(time < 40 || time < 49) {
+  console.log(time + " minutes is teetering on rediculous " + name + " you're most likely a female");
+}
+if(time < 50 || time < 59) {
+  console.log(time + " is way too long to get ready " + name + " you scream female");
+}
+if(time < 60) {
+  console.log(time + " really???? " + name + " do you get lost getting ready?");
+}
+
 
 if (name == "") {
   alert("Please enter a name");
