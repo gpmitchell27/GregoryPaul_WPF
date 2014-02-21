cc3a466fd3125f8e4c8e40dea642586c9495cee1
Gregory Mitchell <gpmitchell27@fullsail.edu>
2014-02-20 21:16:47 -0500

    variable setup
    variables are set up and conditionals are added for different
    calculation

--- Conditionals_Personal/js/scripts.js
+++ Conditionals_Personal/js/scripts.js
@@ -1,2 +1,32 @@
 //Gregory Mitchell Feb 20, 2014 Activity: Conditionals Personal
-//Collect user input for calculating 
+//Collect user info for a purchase
+var x, y;
+var sum, difference, product, quotient, remainder;
+
+// Assign some values to x and y using ASSIGNMENT STATEMENTS and PROMPTS.
+x = parseInt(prompt("Enter a numeric value for x:", ""));
+y = parseInt(prompt("Enter a numeric value for y:", ""));
+if (!x) {
+  prompt("You need to enter a valid number! i.e, 1, 2, 3, 4 etc")
+}
+if (!y) {
+  prompt("You need to enter a valid number! i.e, 1, 2, 3, 4 etc")
+}
+// The prompt function returns strings, use parseInt to change them to numbers
+
+// Use some EXPRESSIONS and ASSIGNMENT STATEMENTS to do arithmetic on the user's data
+// and store values for display.
+
+if(x > y) {
+	console.log("X is greater than Y");
+} else if(x < y) {
+	console.log("X is less than Y");
+} else if(y > x) {
+  console.log("Y is greater than X");
+}
+  else if(y < x) {
+  console.log("Y is less than X");
+} else {
+  console.log("Either you didn't enter a number or the value are the same");
+}
+
