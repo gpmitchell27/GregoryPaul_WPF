a7112c62cdb6f87a91daf6879362ed8d76e99a56
Gregory Mitchell <gpmitchell27@fullsail.edu>
2014-02-20 23:55:02 -0500

    switch statement
    switch statement added trying to get conditionals to work

--- Conditionals_Wacky/js/scripts.js
+++ Conditionals_Wacky/js/scripts.js
@@ -1,28 +1,37 @@
 //Gregory Mitchell Feb 20, 2014 Activity: Conditionals Wacky
 // wacky conditionals body fat index 
 var name = prompt("Please enter your name");
-var time = prompt("On the average, how long does it take you to get ready in the morning? \nTen Minutes, Twenty Minutes, Thrity Minutes, Foutry Minutes, Fifty Minutes, Sixty Plus\n Enter the number in the box");
+var time = parseInt(prompt("On the average, how long does it take you to get ready in the morning?\nTen Minutes, Twenty Minutes, Thrity Minutes, Foutry Minutes, Fifty Minutes, Sixty Plus\nEnter the number in the box"));
 
 switch (time) {
   case 10:
-  alert(time + " minutes is very quick " + name + " there is no way you are female");
+  console.log(time + " minutes is very quick " + name + " there is no way you are female");
+  break;
 
   case 20:
-  alert(time + " minutes is still very quick " + name + " you are most likely a male");
-  
+  console.log(time + " minutes is still very quick " + name + " you are most likely a male");
+  break;
+
   case 30:
-  alert(time + " minutes is average " + name + " you could be a male or female");
-  
+  console.log(time + " minutes is average " + name + " you could be a male or female");
+  break;
+
   case 40:
-  alert(time + " minutes is teetering on rediculous " + name + " you're most likely a female");
-  
+  console.log(time + " minutes is teetering on rediculous " + name + " you're most likely a female");
+  break;
+
   case 50:
-  alert(time + " is way too long to get ready " + name + " you scream female");
-  
+  console.log(time + " is way too long to get ready " + name + " you scream female");
+  break;
+
   case 60:
-  alert(time + " really???? " + name + " do you get lost getting ready?");
-  
+  console.log(time + " really???? " + name + " do you get lost getting ready?");
+  break; 
+}
+
+if (name == "") {
+  alert("Please enter a name");
 }
 if (time == "") {
-  prompt(Enter a number)
+  prompt("Enter a number!");
 }
